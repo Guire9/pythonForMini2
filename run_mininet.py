66587769f8e0a47517c56cd4a32f356b2b24a65e
Guire9 <60325869+Guire9@users.noreply.github.com>
2023-03-28 23:19:23 -0400

    Create run_mininet.py

---
+++ run_mininet.py
@@ -0,0 +1,6 @@
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost, OVSSwitch
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
