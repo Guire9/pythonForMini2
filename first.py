4fed409ca5162113403280a9ca765443215a3e13
Guire9 <60325869+Guire9@users.noreply.github.com>
2023-03-29 23:06:48 -0400

    Update first.py

--- first.py
+++ first.py
@@ -4,7 +4,7 @@ from mininet.node import CPULimitedHost, OVSSwitch
 from mininet.link import TCLink
 import sys
 
-class SingleTopo(Topo):
+class SingleTopology(Topo):
     def build(self, n):
         switch1 = self.addSwitch('s1')
         host1 = self.addHost('h1', cpu=n*0.5)
@@ -14,7 +14,7 @@ class SingleTopo(Topo):
         self.addLink(host2, switch1, bw=10, delay='5ms', loss=10, max_queue_size=1000)
         self.addLink(host3, switch1, bw=10, delay='5ms', loss=10, max_queue_size=1000)
 
-class LinearTopo(Topo):
+class LinearTopology(Topo):
     def build(self, n):
         prev_switch = None
         for i in range(n):
@@ -25,7 +25,7 @@ class LinearTopo(Topo):
                 self.addLink(prev_switch, switch, bw=10, delay='5ms', loss=10, max_queue_size=1000)
             prev_switch = switch
             
-class TreeTopo(Topo):
+class TreeTopology(Topo):
     def build(self, n):
         prev_switch = None
         self.addLink(prev_switch, self.addHost('h1', cpu=.5/n), bw=10, delay='5ms', loss=10, max_queue_size=1000)
@@ -37,7 +37,7 @@ class TreeTopo(Topo):
             self.addLink(switch, self.addHost('h{}'.format(2*i), cpu=.5/n), bw=10, delay='5ms', loss=10, max_queue_size=1000)
             self.addLink(switch, parent, bw=10, delay='5ms', loss=10, max_queue_size=1000)
 
-class MeshTopo(Topo):
+class MeshTopology(Topo):
     def build(self, n):
         self = Mininet(topo=None, host=CPULimitedHost, link=TCLink, switch=OVSSwitch)
         hosts = []
@@ -54,13 +54,13 @@ class MeshTopo(Topo):
         
 def perfTest(topo_type, num_hosts):
     if topo_type == 'single':
-        topo = SingleTopo(num_hosts)
+        topo = SingleTopology(num_hosts)
     elif topo_type == 'linear':
-        topo = LinearTopo(num_hosts)
+        topo = LinearTopology(num_hosts)
     elif topo_type == 'tree':
-        topo = TreeTopo(num_hosts)
+        topo = TreeTopology(num_hosts)
     elif topo_type == 'mesh':
-        topo = MeshTopo(num_hosts)                      
+        topo = MeshTopology(num_hosts)                      
     else:
         print("Invalid topology type: {}".format(topo_type))
         sys.exit(1)
